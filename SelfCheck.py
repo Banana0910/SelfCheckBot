508503e2fd52aa27eac0fb57040c80b329b0d6f6
Banana0910 <38102465+Banana0910@users.noreply.github.com>
2021-09-01 05:06:56 +0900

    Update SelfCheck.py

--- SelfCheck.py
+++ SelfCheck.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from discord.ext import commands,tasks
@@ -11,48 +12,7 @@ studentname = "이대현"
 studentbirthday = "061110"
 studentpw = "0910"
 
-def TryFindElement(xpath) :
-    try :
-        driver.find_element_by_xpath(xpath)
-    except NoSuchElementException :
-        return False
-    return True
-
-def job() :
-    driver.get("https://hcs.eduro.go.kr/#/loginHome")
-    driver.find_element_by_xpath("//*[@id='btnConfirm2']").click()
-    driver.find_element_by_xpath("//*[@id='schul_name_input']").click()
-    driver.find_element_by_xpath("//*[@id='sidolabel']").send_keys(sido)
-    driver.find_element_by_xpath("//*[@id='crseScCode']").send_keys(schoollevel)
-    driver.find_element_by_xpath("//*[@id='orgname']").send_keys(schoolname)
-    driver.find_element_by_xpath("//*[@id='softBoardListLayer']/div[2]/div[1]/table/tbody/tr[3]/td[2]/button").click()
-    time.sleep(0.1)
-    driver.find_element_by_xpath("//*[@id='softBoardListLayer']/div[2]/div[1]/ul/li/a").click()
-    driver.find_element_by_xpath("//*[@id='softBoardListLayer']/div[2]/div[2]/input").click()
-    driver.find_element_by_xpath("//*[@id='user_name_input']").send_keys(studentname)
-    driver.find_element_by_xpath("//*[@id='birthday_input']").send_keys(studentbirthday)
-    driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
-    time.sleep(1)
-    driver.find_element_by_xpath("//*[@id='password']").click()
-    chars = list(studentpw)
-    for c in chars :
-        for i in range(4,10) : 
-            if TryFindElement("//*[@id='password_mainDiv']/div[" + str(i) + "]/a[contains(@aria-label, '" + c + "')]") :
-                driver.find_element_by_xpath("//*[@id='password_mainDiv']/div[" + str(i) + "]/a[contains(@aria-label, '" + c + "')]").click()
-    driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
-    time.sleep(1)
-    driver.find_element_by_class_name("btn").click()
-    time.sleep(1)
-    for i in range(1,4) :
-        driver.find_element_by_xpath("//*[@id='container']/div/div/div[2]/div[2]/dl[" + str(i) + "]/dd/ul/li[1]/label")
-    driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
-    time.sleep(0.5)
-    driver.execute_script("window.history.go(-1)")
-    time.sleep(1)
-    state = driver.find_element_by_class_name("btn").text
-
-    now = datetime.datetime.now()
-    logchannel.send("[" + now.strftime('%Y-%m-%d %H:%M:%S') + "]에 [" + state + "]으로 자가진단을 하였습니다")
+sendmsg = '0'
 
 game = discord.Game("자가진단 대기")
 bot = commands.Bot(command_prefix='!',status=discord.Status.online,activity=game)
@@ -63,27 +23,99 @@ option = webdriver.ChromeOptions()
 option.add_argument("--headless")
 option.add_argument("--disable-gpu")
 option.add_argument("--no-sandbox")
+option.add_argument("--disable-software-rasterizer")
 
-chromepath = os.environ.get('CHROME')
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER"), chrome_options=option)
-logchannel = NULL
+driver = webdriver.Chrome(os.environ['CHROMEDRIVER'],options=option)
+logchannel = None
+
+errored = False
+stack = 0
+
+def TryFindElement(xpath) :
+    try :
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException :
+        return False
+    return True
+
+def job() :
+    global sendmsg,stack,errored
+    try :
+        driver.get("https://hcs.eduro.go.kr/#/loginHome")
+        driver.find_element_by_xpath("//*[@id='btnConfirm2']").click()
+        driver.find_element_by_xpath("//*[@id='schul_name_input']").click()
+        driver.find_element_by_xpath("//*[@id='sidolabel']").send_keys(sido)
+        driver.find_element_by_xpath("//*[@id='crseScCode']").send_keys(schoollevel)
+        driver.find_element_by_xpath("//*[@id='orgname']").send_keys(schoolname)
+        driver.find_element_by_xpath("//*[@id='softBoardListLayer']/div[2]/div[1]/table/tbody/tr[3]/td[2]/button").click()
+        time.sleep(0.1)
+        driver.find_element_by_xpath("//*[@id='softBoardListLayer']/div[2]/div[1]/ul/li/a").click()
+        driver.find_element_by_xpath("//*[@id='softBoardListLayer']/div[2]/div[2]/input").click()
+        driver.find_element_by_xpath("//*[@id='user_name_input']").send_keys(studentname)
+        driver.find_element_by_xpath("//*[@id='birthday_input']").send_keys(studentbirthday)
+        driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
+        time.sleep(1.5)
+        driver.find_element_by_xpath("//*[@id='password']").click()
+        chars = list(studentpw)
+        for c in chars :
+            for i in range(4,10) : 
+                if TryFindElement("//*[@id='password_mainDiv']/div[" + str(i) + "]/a[contains(@aria-label, '" + c + "')]") :
+                    driver.find_element_by_xpath("//*[@id='password_mainDiv']/div[" + str(i) + "]/a[contains(@aria-label, '" + c + "')]").click()
+        driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
+        time.sleep(1)
+        driver.find_element_by_class_name("btn").click()
+        time.sleep(1)
+        for i in range(1,4) :
+            driver.find_element_by_xpath("//*[@id='container']/div/div/div[2]/div[2]/dl[" + str(i) + "]/dd/ul/li[1]/label").click()
+        driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
+        time.sleep(0.5)
+        driver.execute_script("window.history.go(-1)")
+        time.sleep(1)
+        state = driver.find_element_by_class_name("btn").text
+        now = datetime.datetime.now()
+        sendmsg = "[" + now.strftime('%Y-%m-%d %H:%M') + "]에 [" + state + "]으로 자가진단을 처리하였습니다"
+    except Exception:
+        now = datetime.datetime.now()
+        if stack > 2 :
+            sendmsg = "[" + now.strftime('%Y-%m-%d %H:%M') + "] 자가진단 중 3번의 시도에도 불구하고 문제가 발생하여 실패하였습니다"
+            stack = 0
+            return None
+        stack = stack + 1
+        errored = True
+        return None
+
+@bot.event
+async def on_ready():
+    checkpending.start()
 
 @bot.event
 async def on_message(message) :
-    if message.content == '!setchannel' :
+    if message.content == '!set' :
+        global logchannel
         logchannel = message.channel
-        msg = await logchannel.send("자가진단 로그가 이 채널로 설정되었습니다")
+        msg = await logchannel.send("자가진단 로그가 " + str(logchannel) + "(으)로 설정되었습니다")
         time.sleep(5)
-        msg.delete()
+        await msg.delete()
 
 @tasks.loop(seconds=1)
 async def checkpending() :
-    schedule.every().monday.at("08:00").do(job)
-    schedule.every().tuesday.at("08:00").do(job)
-    schedule.every().wednesday.at("08:00").do(job)
-    schedule.every().thursday.at("08:00").do(job)
-    schedule.every().friday.at("08:00").do(job)
+    global logchannel, sendmsg, errored
+    if len(str(sendmsg)) > 2 :
+        await logchannel.send(sendmsg)
+        sendmsg = '0'
+
+    if errored == True :
+        now = datetime.datetime.now()
+        await logchannel.send("[" + now.strftime('%Y-%m-%d %H:%M') + "] 자가진단 중 문제가 발생하였습니다 1초 후에 다시 시도하겠습니다 (중첩 수 : " + str(stack) + ")")
+        errored = False
+        job()
     schedule.run_pending()
-    
+
+schedule.every().monday.at("08:00:00").do(job)
+schedule.every().tuesday.at("08:00:00").do(job)
+schedule.every().wednesday.at("08:00:00").do(job)
+schedule.every().thursday.at("08:00:00").do(job)
+schedule.every().friday.at("08:00:00").do(job)
+
 access_token = os.environ['BOT_TOKEN']
 bot.run(access_token)
