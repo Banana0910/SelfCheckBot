4582465ea8649e2b859446449d031471fc3ba58d
Banana0910 <38102465+Banana0910@users.noreply.github.com>
2021-09-02 05:33:30 +0900

    Update SelfCheck.py

--- SelfCheck.py
+++ SelfCheck.py
@@ -40,49 +40,7 @@ def TryFindElement(xpath) :
 
 def job() :
     global sendmsg,stack,errored
-    try :
-        driver.get("https://hcs.eduro.go.kr/#/loginHome")
-        driver.find_element_by_xpath("//*[@id='btnConfirm2']").click()
-        driver.find_element_by_xpath("//*[@id='schul_name_input']").click()
-        driver.find_element_by_xpath("//*[@id='sidolabel']").send_keys(sido)
-        driver.find_element_by_xpath("//*[@id='crseScCode']").send_keys(schoollevel)
-        driver.find_element_by_xpath("//*[@id='orgname']").send_keys(schoolname)
-        driver.find_element_by_xpath("//*[@id='softBoardListLayer']/div[2]/div[1]/table/tbody/tr[3]/td[2]/button").click()
-        time.sleep(0.1)
-        driver.find_element_by_xpath("//*[@id='softBoardListLayer']/div[2]/div[1]/ul/li/a").click()
-        driver.find_element_by_xpath("//*[@id='softBoardListLayer']/div[2]/div[2]/input").click()
-        driver.find_element_by_xpath("//*[@id='user_name_input']").send_keys(studentname)
-        driver.find_element_by_xpath("//*[@id='birthday_input']").send_keys(studentbirthday)
-        driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
-        time.sleep(1.5)
-        driver.find_element_by_xpath("//*[@id='password']").click()
-        chars = list(studentpw)
-        for c in chars :
-            for i in range(4,10) : 
-                if TryFindElement("//*[@id='password_mainDiv']/div[" + str(i) + "]/a[contains(@aria-label, '" + c + "')]") :
-                    driver.find_element_by_xpath("//*[@id='password_mainDiv']/div[" + str(i) + "]/a[contains(@aria-label, '" + c + "')]").click()
-        driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
-        time.sleep(1)
-        driver.find_element_by_class_name("btn").click()
-        time.sleep(1)
-        for i in range(1,4) :
-            driver.find_element_by_xpath("//*[@id='container']/div/div/div[2]/div[2]/dl[" + str(i) + "]/dd/ul/li[1]/label").click()
-        driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
-        time.sleep(0.5)
-        driver.execute_script("window.history.go(-1)")
-        time.sleep(1)
-        state = driver.find_element_by_class_name("btn").text
-        now = datetime.datetime.now()
-        sendmsg = "[" + now.strftime('%Y-%m-%d %H:%M') + "]에 [" + state + "]으로 자가진단을 처리하였습니다"
-    except Exception:
-        now = datetime.datetime.now()
-        if stack > 2 :
-            sendmsg = "[" + now.strftime('%Y-%m-%d %H:%M') + "] 자가진단 중 3번의 시도에도 불구하고 문제가 발생하여 실패하였습니다"
-            stack = 0
-            return None
-        stack = stack + 1
-        errored = True
-        return None
+    sendmsg = "job 실행됨"
 
 @bot.event
 async def on_ready():
@@ -114,7 +72,7 @@ async def checkpending() :
 schedule.every().monday.at("08:00:00").do(job)
 schedule.every().tuesday.at("08:00:00").do(job)
 schedule.every().wednesday.at("08:00:00").do(job)
-schedule.every().thursday.at("05:30:00").do(job)
+schedule.every().thursday.at("05:45:00").do(job)
 schedule.every().friday.at("08:00:00").do(job)
 
 access_token = os.environ['BOT_TOKEN']
