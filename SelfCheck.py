f723851eda4453da3aef6469ba17ecb5478b80c2
Banana0910 <38102465+Banana0910@users.noreply.github.com>
2021-09-01 16:21:32 +0900

    Update SelfCheck.py

--- SelfCheck.py
+++ SelfCheck.py
@@ -113,7 +113,7 @@ async def checkpending() :
 
 schedule.every().monday.at("08:00:00").do(job)
 schedule.every().tuesday.at("08:00:00").do(job)
-schedule.every().wednesday.at("08:30:00").do(job)
+schedule.every().wednesday.at("16:30:00").do(job)
 schedule.every().thursday.at("08:00:00").do(job)
 schedule.every().friday.at("08:00:00").do(job)
 
