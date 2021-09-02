78929e140cfd3b5f1c5add6633a8474a093d7dd4
Banana0910 <38102465+Banana0910@users.noreply.github.com>
2021-09-02 16:15:29 +0900

    Update SelfCheck.py

--- SelfCheck.py
+++ SelfCheck.py
@@ -121,7 +121,7 @@ async def checkpending() :
 schedule.every().monday.at("08:00:00").do(job)
 schedule.every().tuesday.at("08:00:00").do(job)
 schedule.every().wednesday.at("08:00:00").do(job)
-schedule.every().thursday.at("08:22:00").do(job)
+schedule.every().thursday.at("08:00:00").do(job)
 schedule.every().friday.at("08:00:00").do(job)
 
 access_token = os.environ['BOT_TOKEN']
