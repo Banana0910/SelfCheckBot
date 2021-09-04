b35f09ae5b535b5eeafd349d6c926e1526399e0e
Banana0910 <38102465+Banana0910@users.noreply.github.com>
2021-09-04 09:15:35 +0900

    Update SelfCheck.py

--- SelfCheck.py
+++ SelfCheck.py
@@ -69,7 +69,7 @@ def job() :
         driver.find_element_by_xpath("//*[@id='user_name_input']").send_keys(studentname)
         driver.find_element_by_xpath("//*[@id='birthday_input']").send_keys(studentbirthday)
         driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
-        time.sleep(1.5)
+        time.sleep(5)
         driver.find_element_by_xpath("//*[@id='password']").click()
         chars = list(studentpw)
         for c in chars :
